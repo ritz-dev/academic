c0d6861a92a153e733023c21c71f0e716acf872e
ritz-dev <dev2@ritz.com.mm>
2025-08-08 13:30:25 +0630

    Change academic_class_section_id to academic_class_section slug

--- app/Http/Controllers/APIs/DailyScheduleController.php
+++ app/Http/Controllers/APIs/DailyScheduleController.php
@@ -57,7 +57,7 @@ class DailyScheduleController extends Controller
                         'is_holiday' => true,
                         'holiday_type' => 'weekly',
                         'note' => 'Weekly Holiday',
-                        'academic_info' => null
+                        'academic_info' => 'info'
                     ]);
                 } else {
                     $weeklySlots = WeeklySchedule::where('academic_class_section_slug', $section->id)
@@ -76,7 +76,7 @@ class DailyScheduleController extends Controller
                             'type' => $slot->type,
                             'is_holiday' => false,
                             'note' => null,
-                            'academic_info' => null
+                            'academic_info' => 'info'
                         ]);
                     }
                 }
